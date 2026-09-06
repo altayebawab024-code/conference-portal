e9c53895c89a77fc1de0931d7556a2824afd5466
altayebawab024-code <altayebawab024@gmail.com>
2026-09-06 09:06:57 +0200

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -2,11 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from submissions import views as submissions_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),  # مسارات الدخول للإدارة والشؤون العلمية
-    path('', include('submissions.urls')),                  # مسارات منصة المؤتمرات الشاملة
+    path('accounts/', include('django.contrib.auth.urls')),  # مسارات الدخول والخروج
+    path('', include('submissions.urls')),                  # مسارات منصة المؤتمرات
+
+    # مسار آمن لتحميل وفتح ملفات الأبحاث في الإنتاج على Render
+    path('media/submissions_files/<str:filename>', submissions_views.serve_submission_file, name='serve_submission_file'),
 ]
 
 if settings.DEBUG:
